2c996423c08c3e12be957a0df53268d1c55e215f
James Fletcher <james@grakn.ai>
2021-07-15 17:01:58 +0100

    Assertion was throwing for attribute attachment scenario "when the same attribute is inferred on an entity and relation, both owners are correctly retrieved". The issue identified is that a BoundRetrievableResolver has already sent some answers back before subsumption kicks in, meaning after subsumption kicks in it will always re-send previously seen answers. Therefore a RetrievableResolver should not ask the subsuming Resolver for answers directly, but instead ask the original resolver for answers but tell it where its subsumer is.

--- reasoner/resolution/framework/Request.java
+++ reasoner/resolution/framework/Request.java
@@ -23,6 +23,7 @@ import com.vaticle.typedb.core.reasoner.resolution.answer.AnswerState.Partial;
 
 import javax.annotation.Nullable;
 import java.util.Objects;
+import java.util.Optional;
 
 // TODO: add compute mode: single vs all (for negation vs regular)
 public class Request {
@@ -31,28 +32,36 @@ public class Request {
     private final Actor.Driver<? extends Resolver<?>> receiver;
     private final Partial<?> partialAnswer;
     private final int planIndex;
+    private final Actor.Driver<? extends Resolver<?>> subsumer; // TODO: This should be a separate subclass of request
 
     private final int hash;
 
     private Request(@Nullable Actor.Driver<? extends Resolver<?>> sender, Actor.Driver<? extends Resolver<?>> receiver,
-                    Partial<?> partialAnswer, int planIndex) {
+                    @Nullable Actor.Driver<? extends Resolver<?>> subsumer, Partial<?> partialAnswer, int planIndex) {
         this.sender = sender;
         this.receiver = receiver;
         this.partialAnswer = partialAnswer;
         this.planIndex = planIndex;
-        this.hash = Objects.hash(this.sender, this.receiver, this.partialAnswer);
+        this.subsumer = subsumer;
+        this.hash = Objects.hash(this.sender, this.receiver, this.subsumer, this.partialAnswer);
     }
 
     public static Request create(Actor.Driver<? extends Resolver<?>> sender, Actor.Driver<? extends Resolver<?>> receiver, Partial<?> partialAnswer, int planIndex) {
-        return new Request(sender, receiver, partialAnswer, planIndex);
+        return new Request(sender, receiver, null, partialAnswer, planIndex);
     }
 
     public static Request create(Actor.Driver<? extends Resolver<?>> sender, Actor.Driver<? extends Resolver<?>> receiver, Partial<?> partialAnswer) {
-        return new Request(sender, receiver, partialAnswer, -1);
+        return new Request(sender, receiver, null, partialAnswer, -1);
+    }
+
+    public static Request create(Actor.Driver<? extends Resolver<?>> sender,
+                                 Actor.Driver<? extends Resolver<?>> receiver,
+                                 Actor.Driver<? extends Resolver<?>> subsumer, Partial<?> partialAnswer) {
+        return new Request(sender, receiver, subsumer, partialAnswer, -1);
     }
 
     public static Request create(Actor.Driver<? extends Resolver<?>> receiver, Partial<?> partialAnswer) {
-        return new Request(null, receiver, partialAnswer, -1);
+        return new Request(null, receiver, null, partialAnswer, -1);
     }
 
     public Actor.Driver<? extends Resolver<?>> receiver() {
@@ -61,6 +70,10 @@ public class Request {
 
     public Actor.Driver<? extends Resolver<?>> sender() {
         return sender;
+    }
+
+    public Optional<Actor.Driver<? extends Resolver<?>>> subsumer() {
+        return Optional.ofNullable(subsumer);
     }
 
     public Partial<?> partialAnswer() {
@@ -78,6 +91,7 @@ public class Request {
         Request request = (Request) o;
         return Objects.equals(sender, request.sender) &&
                 Objects.equals(receiver, request.receiver) &&
+                Objects.equals(subsumer, request.subsumer) &&
                 Objects.equals(partialAnswer, request.partialAnswer());
     }
 
@@ -91,6 +105,7 @@ public class Request {
         return "Request{" +
                 "sender=" + sender +
                 ", receiver=" + receiver +
+                ", subsumer=" + subsumer +
                 ", partial=" + partialAnswer +
                 '}';
     }
